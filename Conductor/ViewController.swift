1992a80efc32f5be54cf7d5516c769eb1756daa4
Pranav Jain <pranajain@gmail.com>
2016-03-15 02:19:00 -0400

    Code to receive gps information and print it out.

--- Conductor/ViewController.swift
+++ Conductor/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var gpsLabel: UILabel!
+    @IBOutlet weak var coord: UITextView!
+    
+    var locationManager:CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("locations = \(locations)")
+        let latitude: (Double!) = locations.first?.coordinate.latitude
+        let longitude: (Double!) = locations.first?.coordinate.longitude
+        let speed: (Double!) = locations.first?.speed
+        gpsLabel.text = "Success"
+        coord.text = "Latitude: \(latitude)\nLongitude: \(longitude)\nSpeed: \(speed) m/s"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +37,3 @@ class ViewController: UIViewController {
 
 
 }
-
